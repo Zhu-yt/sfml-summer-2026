03cbcbfb0d58cb2b3ab0c33c6faec190204697a8
Zhu-yt <13798337794@163.com>
2026-07-27 01:38:20 +0800

    refactor: extract Game class from main.cpp

---
+++ 01-SpaceShooter/src/Game.cpp
@@ -0,0 +1,115 @@
+#include "Game.hpp"
+
+const float Game::PlayerSpeed = 300.f;
+
+Game::Game()
+: mWindow(sf::VideoMode(800, 600), "Space Shooter - Week 1")
+, mPlayer(sf::Vector2f(50.f, 50.f))
+, mIsMovingLeft(false)
+, mIsMovingRight(false)
+, mIsMovingUp(false)
+, mIsMovingDown(false)
+{
+    mWindow.setFramerateLimit(60);
+    mPlayer.setFillColor(sf::Color::Green);
+    mPlayer.setPosition(375.f, 500.f); //飞机起始放在底部中间
+}
+
+void Game::run(){
+    sf::Clock clock;
+
+    while(mWindow.isOpen()){
+        processEvents();
+
+        sf::Time deltaTime = clock.restart();
+
+        update(deltaTime);
+        render();
+    }
+}
+
+void Game::processEvents(){
+    //事件处理
+    sf::Event event;
+    while(mWindow.pollEvent(event)){
+        switch(event.type){
+            case sf::Event::KeyPressed:
+                if(event.key.code == sf::Keyboard::A){
+                    mIsMovingLeft = true;
+                }
+                if(event.key.code == sf::Keyboard::D){
+                    mIsMovingRight = true;
+                }
+                if(event.key.code == sf::Keyboard::W){
+                    mIsMovingUp = true;
+                }
+                if(event.key.code == sf::Keyboard::S){
+                    mIsMovingDown = true;
+                }
+                break;
+            
+            case sf::Event::KeyReleased:
+                if(event.key.code == sf::Keyboard::A){
+                    mIsMovingLeft = false;
+                }
+                if(event.key.code == sf::Keyboard::D){
+                    mIsMovingRight = false;
+                }
+                if(event.key.code == sf::Keyboard::W){
+                    mIsMovingUp = false;
+                }
+                if(event.key.code == sf::Keyboard::S){
+                    mIsMovingDown = false;
+                }
+                break;
+
+            case sf::Event::Closed:
+                mWindow.close();
+                break;
+
+        }
+    }
+}
+
+void Game::update(sf::Time deltaTime){
+    //移动逻辑
+    float dt = deltaTime.asSeconds();
+
+    if(mIsMovingLeft){
+        mPlayer.move(-PlayerSpeed * dt, 0.f);
+    }
+    if(mIsMovingRight){
+        mPlayer.move(PlayerSpeed * dt, 0.f);
+    }
+    if(mIsMovingUp){
+        mPlayer.move(0.f, -PlayerSpeed * dt);
+    }
+    if(mIsMovingDown){
+        mPlayer.move(0.f, PlayerSpeed * dt);
+    }
+
+    //边界限制
+    sf::Vector2f pos = mPlayer.getPosition();
+
+    if(pos.x < 0.f){
+        pos.x = 0.f;
+    }
+    if(pos.x > 750.f){
+        pos.x = 750.f;
+    }
+    if(pos.y < 0.f){
+        pos.y = 0.f;
+    }
+    if(pos.y > 550.f){
+        pos.y = 550.f;
+    }
+
+    mPlayer.setPosition(pos);
+}
+
+void Game::render(){
+    //渲染
+    mWindow.clear(sf::Color::Black);
+    mWindow.draw(mPlayer);
+    mWindow.display();
+}
---
+++ 01-SpaceShooter/src/Game.hpp
@@ -0,0 +1,29 @@
+#ifndef GAME_HPP
+#define GAME_HPP
+
+#include <SFML/Graphics.hpp>
+
+class Game{
+    public:
+        Game();
+        void run();
+
+    private:
+        void processEvents();
+        void update(sf::Time deltaTime);
+        void render();
+
+        sf::RenderWindow mWindow;
+
+        sf::RectangleShape mPlayer;
+
+        bool mIsMovingLeft;
+        bool mIsMovingRight;
+        bool mIsMovingUp;
+        bool mIsMovingDown;
+
+        static const float PlayerSpeed;
+
+};
+
+#endif // GAME_HPP
